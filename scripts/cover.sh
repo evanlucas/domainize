98a59b4a08a338cc19715a40dab29296b1d2342b
Evan Lucas <evanlucas@me.com>
2014-05-27 22:04:58 -0500

    Remove scripts
    No longer used

--- scripts/cover.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf lib-cov
-jscoverage lib/ lib-cov/
-DOMAINIZE_COV=1 mocha -R html-cov test/*.js > coverage.html
-DOMAINIZE_COV=1 mocha -R json-cov test/*.js > coverage.json
